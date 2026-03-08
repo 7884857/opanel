6daa15393ae0c36eab36ce42518c1f124957d9fb
NriotHrreion <nriot233@outlook.com>
2026-03-08 18:15:07 +0800

    style: Use constant to store port number

--- pumpkin/web.rs
+++ pumpkin/web.rs
@@ -3,18 +3,20 @@ use tokio::net::TcpListener;
 
 use crate::handlers::hello::*;
 
+const PORT: usize = 3000;
+
 pub async fn init_web_server() -> Result<(), String> {
   let app = Router::new()
     .route("/", get(hello_handler));
 
-  let listener = TcpListener::bind("0.0.0.0:3000")
+  let listener = TcpListener::bind(format!("0.0.0.0:{}", PORT))
     .await
     .unwrap();
   axum::serve(listener, app)
     .await
     .unwrap();
 
-  tracing::info!("OPanel web server is ready on port 3000");
+  tracing::info!("OPanel web server is ready on port {}", PORT);
 
   Ok(())
 }
